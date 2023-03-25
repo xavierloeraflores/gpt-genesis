57d62c3e27da5857006c6dd4223ee52761a91a9d
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-03-25 16:52:59 -0700

    index header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import Sandbox from "npm/components/sandbox/sandbox";
+import SearchBar from "npm/components/searchbar/searchbar";
 
 import { api } from "npm/utils/api";
 
@@ -16,6 +17,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        <header>
+          <h1 className="text-6xl font-bold">GPT Genesis</h1>
+          <h2 className="text-center text-2xl font-bold">Wikis by ChatGPT</h2>
+          <h3 className="text-center">Created by Xavier Loera Flores</h3>
+        </header>
+        <SearchBar />
         <span>Beginning of Sandbox</span>
         <Sandbox />
       </main>
