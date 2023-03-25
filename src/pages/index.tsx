e67ef7e5b2737e89c072800b62c62bebfd54025d
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-03-25 16:12:45 -0700

    made the background white

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         <meta name="description" content="GPT Genesis - Wikis by ChatGPT" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-200">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
         <span>Beginning of Sandbox</span>
         <Sandbox />
       </main>
