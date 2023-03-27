842c32174593fa5d9d3a52d861d8d6ad77850494
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-03-27 13:50:58 -0700

    added open ai text completion on the backend

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,13 +1,26 @@
 import SearchBar from "npm/components/searchbar/searchbar";
 import { useState } from "react";
 
+import { api } from "npm/utils/api";
 import SearchResultList from "../searchresultlist/searchresultlist";
+import Button from "../button/button";
 
 const Search: React.FC = () => {
   const [searchText, setSearchText] = useState("");
+  const { mutate, isLoading } = api.articles.generate.useMutation({
+    onSuccess: (data) => {
+      console.log("Success!");
+      console.log({ data });
+    },
+    onError: (error) => {
+      console.log("Error!");
+      console.log({ error });
+    },
+  });
   return (
     <div>
       <SearchBar setParentSearchText={setSearchText} />
+      <Button text="Generate!" onClick={() => mutate({ text: searchText })} />
       <SearchResultList searchText={searchText} />
     </div>
   );
--- src/server/openai.ts
+++ src/server/openai.ts
@@ -4,7 +4,27 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: env.OPENAI_API_KEY,
 });
-
+// gpt-3.5-turbo
 const openai = new OpenAIApi(configuration);
+
+export interface CompletionResponse {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  choices: [
+    {
+      text: string;
+      index: number;
+      logprobs: null;
+      finish_reason: string;
+    }
+  ];
+  usage: {
+    prompt_tokens: number;
+    completion_tokens: number;
+    total_tokens: number;
+  };
+}
 
 export default openai;
