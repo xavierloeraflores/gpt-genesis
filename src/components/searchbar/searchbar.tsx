87d612ef5d702205c43fde6aa0cf35c781b692dd
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-03-27 13:55:43 -0700

    removed extra generate button

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -7,17 +7,14 @@ export interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ setParentSearchText }) => {
   return (
-    <div className="flex w-full rounded-md">
-      <input
-        type="text"
-        placeholder="Search 🔎"
-        className="grow rounded-md border bg-transparent bg-white p-1 outline-none"
-        onChange={(e) => {
-          setParentSearchText(e.target.value);
-        }}
-      />
-      <Button text="Generate" onClick={() => console.log("generating")} />
-    </div>
+    <input
+      type="text"
+      placeholder="Search 🔎"
+      className="grow rounded-md border bg-transparent bg-white p-1 outline-none"
+      onChange={(e) => {
+        setParentSearchText(e.target.value);
+      }}
+    />
   );
 };
 export default SearchBar;
