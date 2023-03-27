20c75215a6c0209a7d2e7d774bcd30e40b370043
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-03-27 11:46:53 -0700

    changed button sizing

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,10 +7,12 @@ const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="group relative mx-0.5 w-16 overflow-hidden rounded-sm bg-white"
+      className="group relative mx-0.5 min-w-fit max-w-sm overflow-hidden rounded-md bg-white"
     >
       <div className="absolute inset-0 w-2 bg-black transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-      <span className="relative text-black group-hover:text-white">{text}</span>
+      <span className="relative mx-2 text-black group-hover:text-white">
+        {text}
+      </span>
     </button>
   );
 };
