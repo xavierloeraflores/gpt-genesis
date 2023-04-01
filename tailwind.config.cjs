bc16dc133552307c7862eadf91e3154ccf6e4b9d
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-03-31 23:15:47 -0700

    added slate to color pallet

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { slate } = require("@radix-ui/colors");
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...slate,
+      },
+    },
   },
   plugins: [],
 };
