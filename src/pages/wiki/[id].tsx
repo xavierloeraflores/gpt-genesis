7014e0c27296be4c603136de12b8a2d5bcf955ed
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-04-01 23:08:07 -0700

    added generating logic

--- src/pages/wiki/[id].tsx
+++ src/pages/wiki/[id].tsx
@@ -1,7 +1,39 @@
-import { NextPage } from "next";
+import { NextPage, GetServerSideProps } from "next";
 import Head from "next/head";
+import { api } from "npm/utils/api";
+import { useRouter } from "next/router";
+
+interface Article {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+interface ArticleProps {
+  article: Article;
+}
 
 const Wiki: NextPage = () => {
+  const router = useRouter();
+  const id: string = router.query.id as string;
+  const response = api.articles.getArticleById.useQuery({ id });
+  if (!response || response.error || !response.data) {
+    return <div>Error</div>;
+  }
+  const article = response?.data;
+
+  if (article.generated == 0) {
+    return (
+      <>
+        <h1>Generating</h1>
+        {
+          // setTimeout(() => {router.reload();}, 3000)
+        }
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
@@ -17,9 +49,31 @@ const Wiki: NextPage = () => {
       </header>
       <main>
         <h1>Wiki</h1>
+        <h2>{article.title}</h2>
       </main>
     </>
   );
 };
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   // const router = useRouter();
+//   // const id:string = router.query.id as string;
+//   //   const id: string = (context.params?.id as string) || "";
+//   const id: string = (context.params?.id as string) || "";
+
+//   const article = api.articles.getArticleById.useQuery({ id });
+//   if (article.error) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   return {
+//     notFound: false,
+//     props: {
+//       article: article.data,
+//     },
+//   };
+// };
 
 export default Wiki;
