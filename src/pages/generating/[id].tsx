c341454c610079b096d0b724455dd2f3ed213bd3
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-04-02 17:24:31 -0700

    added progress bar to generating page

--- src/pages/generating/[id].tsx
+++ src/pages/generating/[id].tsx
@@ -2,16 +2,18 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { api } from "npm/utils/api";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import * as Progress from "@radix-ui/react-progress";
 
 const Generating: NextPage = () => {
-  console.log("Generating page");
+  const [progress, setProgress] = useState(0);
   const router = useRouter();
   const id: string = router.query.id as string;
 
   const { mutate } = api.articles.generate.useMutation({
     onSuccess: (data) => {
       console.log("Successful Page Generation!");
+      setProgress(100);
       void router.push(`/wiki/${id}`);
     },
     onError: (error) => {
@@ -19,6 +21,12 @@ const Generating: NextPage = () => {
       console.log({ error });
     },
   });
+  useEffect(() => {
+    if (progress < 90) {
+      const timer = setTimeout(() => setProgress(progress + 10), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [progress]);
 
   useEffect(() => {
     console.log("Generating page useEffect");
@@ -39,6 +47,20 @@ const Generating: NextPage = () => {
       </header>
       <main>
         <h1>Generating</h1>
+        <Progress.Root
+          className="relative h-[25px] w-[300px] overflow-hidden rounded-full bg-blackA9"
+          style={{
+            // Fix overflow clipping in Safari
+            // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
+            transform: "translateZ(0)",
+          }}
+          value={progress}
+        >
+          <Progress.Indicator
+            className="ease-[cubic-bezier(0.65, 0, 0.35, 1)]  h-full w-full bg-green-500 transition-transform duration-[660ms]"
+            style={{ transform: `translateX(-${100 - progress}%)` }}
+          />
+        </Progress.Root>
       </main>
     </>
   );
