09d7ccd9e5ce8e03a397961e57930fe8663f86ac
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-04-06 00:46:53 -0700

    REFACTOR: made main the top level element for each NextPage component'

--- src/pages/generating/[id].tsx
+++ src/pages/generating/[id].tsx
@@ -79,32 +79,28 @@ const Generating: NextPage = () => {
   }, [progress, id, router]);
 
   return (
-    <>
+    <main>
       <Head>
         <title>GPT Genesis</title>
         <meta name="description" content="GPT Genesis - Wikis by ChatGPT" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header />
-      <main>
-        <h1>Generating</h1>
-        <Progress.Root
-          className="relative h-[25px] w-[300px] overflow-hidden rounded-full bg-blackA9"
-          style={{
-            // Fix overflow clipping in Safari
-            // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
-            transform: "translateZ(0)",
-          }}
-          value={progress}
-        >
-          <Progress.Indicator
-            className="ease-[cubic-bezier(0.65, 0, 0.35, 1)]  h-full w-full bg-green-500 transition-transform duration-[660ms]"
-            style={{ transform: `translateX(-${100 - progress}%)` }}
-          />
-        </Progress.Root>
-      </main>
-      <Footer />
-    </>
+      <h1>Generating</h1>
+      <Progress.Root
+        className="relative h-[25px] w-[300px] overflow-hidden rounded-full bg-blackA9"
+        style={{
+          // Fix overflow clipping in Safari
+          // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
+          transform: "translateZ(0)",
+        }}
+        value={progress}
+      >
+        <Progress.Indicator
+          className="ease-[cubic-bezier(0.65, 0, 0.35, 1)]  h-full w-full bg-green-500 transition-transform duration-[660ms]"
+          style={{ transform: `translateX(-${100 - progress}%)` }}
+        />
+      </Progress.Root>
+    </main>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,15 @@ import Search from "npm/components/search/search";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <main className="flex min-h-screen flex-col items-center bg-gray-100">
       <Head>
         <title>GPT Genesis</title>
         <meta name="description" content="GPT Genesis - Wikis by ChatGPT" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center bg-gray-100">
-        <Search />
-        <Sandbox />
-      </main>
-    </>
+      <Search />
+      <Sandbox />
+    </main>
   );
 };
 
