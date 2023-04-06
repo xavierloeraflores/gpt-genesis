816b7b0257e6dec801c5f7e1973d1a561d33c262
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-04-05 21:26:05 -0700

    FEAT: Added layout component to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { api } from "npm/utils/api";
 
 import "npm/styles/globals.css";
+import Layout from "npm/components/layout/layout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 };
 
 export default api.withTRPC(MyApp);
