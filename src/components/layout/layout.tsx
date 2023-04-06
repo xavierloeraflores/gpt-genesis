a405c783ff8bfeec293820c61ec461808f9e6e2e
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-04-05 21:25:38 -0700

    FEAT: Created a layout component

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "../header/header";
+import Footer from "../Footer/footer";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
